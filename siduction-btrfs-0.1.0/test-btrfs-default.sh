658396b154791b868b780b755c5ae81a7bd34446
Axel Konrad <emma-konrad@web.de>
2023-02-26 20:35:29 +0100

    Build debian package

---
+++ siduction-btrfs-0.1.0/test-btrfs-default.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+#
+# Name: /usr/share/siduction/test-btrfs-default.sh
+# Part of siduction-btrfs
+# Called by /usr/lib/systemd/system/siduction_btrfs.service
+# After booting or new snapshot, checks if Btrfs default subvolume, booted subvolume, and Grub default menu item match.
+# Create new /boot/grub/grub.cfg if not.
+
+sleep 10
+btrfs_default=$(echo "$(btrfs subvolume get-default /)" | sed -E 's,[^@]*@?,@,')
+booted_subvol=$(btrfs inspect-internal subvolid-resolve $(btrfs inspect-internal rootid /) /)
+grub_default_menu=$(grep -m 1 -o '/.*/boot/vmlinuz' /boot/grub/grub.cfg | sed 's,/\(.*\)/boot/vmlinuz,\1,')
+
+if [ "x${btrfs_default}" = "x${booted_subvol}" ]; then
+    if [ "x${btrfs_default}" = "x${grub_default_menu}" ]; then
+        echo "Nothing to do"
+        exit 0
+    else
+        echo "Btrfs default subvolume and Grub default menu item differ."
+        echo "Start \"update-grub\""
+        update-grub
+    fi
+else
+    echo "Btrfs default and login subvolume differ." 
+    echo "Start \"update-grub\""
+    update-grub
+fi
---
+++ siduction-btrfs_0.1.0-1.dsc
@@ -0,0 +1,21 @@
+Format: 3.0 (quilt)
+Source: siduction-btrfs
+Binary: siduction-btrfs
+Architecture: all
+Version: 0.1.0-1
+Maintainer: Axel Konrad <emma-konrad@web.de>
+Homepage: https://github.com/siduction/grub-btrfs-rollback_settings
+Standards-Version: 4.6.2
+Testsuite: autopkgtest
+Build-Depends: debhelper-compat (= 13)
+Package-List:
+ siduction-btrfs deb utils optional arch=all
+Checksums-Sha1:
+ 6dd7c05ac0a62f8012c4cf2a679821b4dc40bd7f 5019 siduction-btrfs_0.1.0.orig.tar.gz
+ 1fdf9004f8689ff8691e57f9c4e94edab45e9e5b 2620 siduction-btrfs_0.1.0-1.debian.tar.xz
+Checksums-Sha256:
+ 39314de55ef54b1bf26d9cd7ecd96d5fd8ecb6377275b7b546386659df3d1de1 5019 siduction-btrfs_0.1.0.orig.tar.gz
+ 195977638a38aabf204b5cd7462b58d96f862fd53d338576e5ce061d9ca1b999 2620 siduction-btrfs_0.1.0-1.debian.tar.xz
+Files:
+ 0b2f6844b81703529ae18e13a2152b43 5019 siduction-btrfs_0.1.0.orig.tar.gz
+ 7f54f75428389f8d340ffc3326f5bdd2 2620 siduction-btrfs_0.1.0-1.debian.tar.xz
