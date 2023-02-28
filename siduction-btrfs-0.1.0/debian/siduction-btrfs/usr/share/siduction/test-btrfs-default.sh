c3d0bdad059d76a97587302d579d510acfb8b7d1
Axel Konrad <emma-konrad@web.de>
2023-02-28 22:19:17 +0100

    Move the / filesystem type test into test-btrfs-default.sh

--- siduction-btrfs-0.1.0/debian/siduction-btrfs/usr/share/siduction/test-btrfs-default.sh
+++ siduction-btrfs-0.1.0/debian/siduction-btrfs/usr/share/siduction/test-btrfs-default.sh
@@ -3,29 +3,43 @@
 # Name: /usr/share/siduction/test-btrfs-default.sh
 # Part of siduction-btrfs
 # Called by /usr/lib/systemd/system/siduction_btrfs.service
-# After booting or new snapshot, checks if Btrfs default subvolume, booted subvolume, and Grub default menu item match.
+# After booting or new snapshot, checks if Btrfs default subvolume,
+# booted subvolume, and Grub default menu item match.
 # Create new /boot/grub/grub.cfg if not.
 
 set -e
 
-sleep 10
-btrfs_default=$(echo "$(btrfs subvolume get-default /)" | sed -E 's,[^@]*@?,@,')
-booted_subvol=$(btrfs inspect-internal subvolid-resolve $(btrfs inspect-internal rootid /) /)
-grub_default_menu=$(grep -m 1 -o '/.*/boot/vmlinuz' /boot/grub/grub.cfg | sed 's,/\(.*\)/boot/vmlinuz,\1,')
+# Test whether file system of "/" is btrfs.
+ROOTFSTYP=$( grep '[[:space:]]\+/[[:space:]]\+' /etc/fstab | awk '{ print $3 }' )
 
-if [ "x${btrfs_default}" = "x${booted_subvol}" ]; then
-    if [ "x${btrfs_default}" = "x${grub_default_menu}" ]; then
-        echo "Nothing to do"
-        exit 0
+if [ "x$ROOTFSTYP" = "xbtrfs" ]; then
+    # If the file system is btrfs, then do the work.
+    
+    sleep 10
+    
+    # Query the btrfs default subvolume, the booted subvolume, and the grub default menu entry.
+    btrfs_default=$(echo "$(btrfs subvolume get-default /)" | sed -E 's,[^@]*@?,@,')
+    booted_subvol=$(btrfs inspect-internal subvolid-resolve $(btrfs inspect-internal rootid /) /)
+    grub_default_menu=$(grep -m 1 -o '/.*/boot/vmlinuz' /boot/grub/grub.cfg | sed 's,/\(.*\)/boot/vmlinuz,\1,')
+
+    # Compare the query values and execute action if necessary.
+    if [ "x${btrfs_default}" = "x${booted_subvol}" ]; then
+        if [ "x${btrfs_default}" = "x${grub_default_menu}" ]; then
+            echo "Nothing to do"
+            exit 0
+        else
+            echo "Btrfs default subvolume and Grub default menu item differ."
+            echo "Start \"update-grub\""
+            update-grub
+        fi
     else
-        echo "Btrfs default subvolume and Grub default menu item differ."
+        echo "Btrfs default and login subvolume differ." 
         echo "Start \"update-grub\""
         update-grub
     fi
 else
-    echo "Btrfs default and login subvolume differ." 
-    echo "Start \"update-grub\""
-    update-grub
+    # No btrfs: Disable systemd units and we never see each other here again.
+    systemctl mask --now siduction_btrfs.timer siduction_btrfs.path
 fi
 
 exit 0
---
+++ siduction-btrfs_0.1.0-2.dsc
@@ -0,0 +1,21 @@
+Format: 3.0 (quilt)
+Source: siduction-btrfs
+Binary: siduction-btrfs
+Architecture: all
+Version: 0.1.0-2
+Maintainer: Axel Konrad <emma-konrad@web.de>
+Homepage: https://github.com/siduction/grub-btrfs-rollback_settings
+Standards-Version: 4.6.2
+Testsuite: autopkgtest
+Build-Depends: debhelper-compat (= 13)
+Package-List:
+ siduction-btrfs deb utils optional arch=all
+Checksums-Sha1:
+ 2ab252b724fb69eccca91283916eb9270d8886dc 5257 siduction-btrfs_0.1.0.orig.tar.gz
+ 3637a4b21f9af3a3f225b0c294f4a7081b654ec7 2760 siduction-btrfs_0.1.0-2.debian.tar.xz
+Checksums-Sha256:
+ bfa18f277b7497d9d03e22a1555075e41afb779efbc4d9081a60ac2a64aefcc0 5257 siduction-btrfs_0.1.0.orig.tar.gz
+ 9d2d1de1239fcaf79d632757a100e56589ce2e6077152547cfceda5df1e87755 2760 siduction-btrfs_0.1.0-2.debian.tar.xz
+Files:
+ c9793d586611714faeab52624249cc1f 5257 siduction-btrfs_0.1.0.orig.tar.gz
+ 079bc328c5aab42e3625d72626128af2 2760 siduction-btrfs_0.1.0-2.debian.tar.xz
