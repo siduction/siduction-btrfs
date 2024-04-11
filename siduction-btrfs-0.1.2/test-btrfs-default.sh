ba6c314a38b8e3221021dec2820844f68f808ce6
Axel Konrad <axel.konrad@mailbox.org>
2024-04-11 23:05:28 +0200

    Enhancement: Editing /etc/fstab in the rollback target.

--- siduction-btrfs-0.1.2/test-btrfs-default.sh
+++ siduction-btrfs-0.1.2/test-btrfs-default.sh
@@ -27,13 +27,15 @@ fi
 # Query the btrfs default subvolume, the booted subvolume, and the grub default menu entry.
 # 1) Subvolume, that is set to default in Btrfs
 btrfs_default=$(echo "$btrfs_default" | sed -E 's,[^@]*@?,@,')
+echo "btrfs default: $btrfs_default"
 
 # 2) Subvolume that was booted into.
 booted_subvol=$(btrfs inspect-internal subvolid-resolve $(btrfs inspect-internal rootid /) /)
+echo "booted subvol: $booted_subvol"
 
 # 3) Subvolume of the grub default boot entry.
 grub_default_menu=$(grep -m 1 -o '/.*/boot/vmlinuz' /boot/grub/grub.cfg | sed 's,/\(.*\)/boot/vmlinuz,\1,')
-
+echo "grub default: $grub_default_menu"
 
 # Compare the query values and execute action if necessary.
 if [ "x${btrfs_default}" = "x${booted_subvol}" ]; then
@@ -43,7 +45,7 @@ if [ "x${btrfs_default}" = "x${booted_subvol}" ]; then
         exit 0
     else
     
-    # Btrfs subvolume and booted subvolume are the same.
+    # Btrfs default subvolume and booted subvolume are the same.
     # The grub standard boot entry differs.
     # State after booting into the rollback target first time.
         echo "Btrfs default subvolume and Grub default menu item differ."
@@ -76,11 +78,38 @@ if [ "x${btrfs_default}" = "x${booted_subvol}" ]; then
 else
     
     # State after rollback and without reboot.
-    # We still have write permissions in the previous default
-    # subvolume, in which we are currently located.
+    # We still have write permissions in the new
+    # default subvolume, and in the previous default
+    # subvolume, in which we are currently located
     echo "Btrfs default and booted subvolume differ." 
     echo "Run \"update-grub\""
     update-grub
+    
+    # We have to edit /etc/fstab in the rollback target
+    # Query whether the default subvolume was set to @.
+    # (E.g. with a btrfs command instead of a snapper rollback).
+    if [ "x$btrfs_default" = "x@" ]; then
+        target_path="" 
+    else
+        target_path=$(echo "$btrfs_default" | sed 's!@!/.!')
+    fi
+
+    # Backup fstab. Remove old backup first.
+    oldbak=$(find "$target_path"/etc/* -maxdepth 0 -regex ".*fstab_btrfs_.*" 2>/dev/null)
+    if [ "$oldbak" ]; then
+        echo "Remove old fstab backup."
+	rm $(echo "$oldbak")
+    fi
+
+    newbak="fstab_btrfs_$(date +%F_%H%M.bak)"
+    echo "Backup fstab in the rollback target to $newbak"
+    cp "$target_path"/etc/fstab "$target_path"/etc/"$newbak"
+
+    # Now edit .../etc/fstab 
+    if [ -w "$target_path"/etc/fstab 2>/dev/null ]; then
+        echo "Edit fstab in the rollback target."
+        sed -i 's!^\(.* / .*subvol=/\)@[^,]*,\(.*\)!\1'"$btrfs_default"',\2!' "$target_path"/etc/fstab
+    fi
 fi
 
 exit 0
