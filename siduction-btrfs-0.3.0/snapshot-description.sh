11d611381b5aae8781b5a7e7a764913a662a3b96
Axel Konrad <axel.konrad@mailbox.org>
2025-01-02 19:16:10 +0100

    Changes due to modified apt package.

--- siduction-btrfs-0.3.0/snapshot-description.sh
+++ siduction-btrfs-0.3.0/snapshot-description.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 #
 # Name: /usr/share/siduction/snapshot-description.sh
 # Part of siduction-btrfs
@@ -12,29 +12,59 @@ post_num="$1"
 ### Begin funktions ###
 
 find_package () {
-# Search for the first package and the total number of packages
-# for the given action.
-# Snapper outputs the values in the description.
+# Search for the first package of the given action.
+# Read the first package.from the command line.
+if grep -P -q " (apt-get|apt) " <<< $2; then
+	apt_package=$(sed -e 's|apt-get | |' \
+		-e 's|apt | |' \
+		-e 's| --solver 3.0||' \
+		-e 's| -\+[^ ]*||g' \
+		-e "s|$3||" \
+		-e 's|^ \+||' \
+		-e 's|^\([^ ]*\)|\1|' <<< $2 | \
+		cut -d " " -f 1 | \
+		grep -o -P "^[a-z0-9_-]+.[a-z0-9]+" )
+fi
 
-# Reading in the package line.
+# Read in the package line.
 if pkg=$(grep "$1" "$TEMP1"); then
 	# Cut out the given action.
 	pkg=${pkg#$1: }
 
-	# Read out the first package.
-	apt_package=$(sed 's|[- ].*||' <<< ${pkg%%:*})
-
-	# Counting the total number of packages.
-	for i in $pkg; do
-		if [ $(grep '^[a-z]' <<< $i) ]; then
-			((++count))
-		fi
-	done
 else
-	# Reverts to the original command if apt modifies the
-	# command internally and therefore no package was found.
-	apt_package=" "
+
+# APT internally changed the command.
+# Search for the command used.
+	if [ "X$1" = "XInstall" ]; then
+		if pkg=$(grep "Upgrade" "$TEMP1"); then
+			pkg=${pkg#Upgrade: }
+		fi
+	elif [ "X$1" = "XReinstall" ]; then
+		if pkg=$(grep "Install" "$TEMP1"); then
+			pkg=${pkg#Install: }
+		elif pkg=$(grep "Upgrade" "$TEMP1"); then
+			pkg=${pkg#Upgrade: }
+		fi
+	elif [ "X$1" = "XRemove" ]; then
+		if pkg=$(grep "Purge" "$TEMP1"); then
+			pkg=${pkg#Purge: }
+		fi
+	fi
 fi
+count_packages
+}
+
+count_packages () {
+# Cleanup extended package description and
+# counting the total number of packages.
+
+pkg=$(sed 's| ([^)]*),\?||g' <<< $pkg)
+
+for i in $pkg; do
+	if [ $(grep '^[a-z]' <<< "$i") ]; then
+		((++count))
+	fi
+done
 }
 
 ### End funktions ###
@@ -51,7 +81,7 @@ if snapper_last_post=$(snapper --no-headers --machine-readable csv list \
 	# The required variables are filled with the values from snapper.
 	post_date=$(echo "$snapper_last_post" | cut -d "," -f 8 | sed 's![: -]!!g')
 	pre_date=$(echo "$snapper_last_pre" | cut -d "," -f 8 | sed 's![: -]!!g')
-	pre_num=$post_num && ((--pre_num))
+	pre_num=$(( "$post_num" - 1 ))
 else
 	# This is not an apt post snapshot.
 	echo "$(date  +%T) snapshot-description: No complete apt action." >> /var/log/snapper.log
@@ -82,58 +112,67 @@ count=0
 
 apt_start=$(grep "Start-Date" "$TEMP1" | sed -e 's!^Start-Date!!' -e 's![: -]!!g')
 apt_end=$(grep "End-Date" "$TEMP1" | sed -e 's!^End-Date!!' -e 's![: -]!!g')
-apt_full_command=$(grep "Commandline" "$TEMP1" | sed 's!Commandline: !!')
+apt_full_command=$(grep "Commandline" "$TEMP1" | sed 's!Commandline:!!')
 
 # Search for matching apt actions.
 # Prepare the first part of the snapper output.
 # Declare the search pattern.
-pattern=("apt-get remove --purge --yes linux-" ".*autoremove" ".*purge" \
-".*remove" ".*reinstall" ".*install" ".*-upgrade" ".*upgrade" ".*synaptic")
+pattern=("apt-get remove --purge --yes linux-" " autoremove" " autopurge" " purge" \
+" remove" " reinstall" " install" ".*-upgrade" " upgrade" " synaptic")
 
 for value in "${pattern[@]}"; do
 	if grep -q -P "$value" <<< "$apt_full_command"; then
-		apt_command="$value"
 		break
 	fi
 done
 
-case "$apt_command" in
+case "$value" in
 "apt-get remove --purge --yes linux-")
 	apt_command="kernel-rm"
 	apt_package=$( grep -o "image[[:print:]]\+[a-z]" <<< "$apt_full_command" \
 	| grep -o "[.0-9]\+-[0-9]")
 	;;
-".*autoremove")
+" autoremove")
 	apt_command="Remove"
-	find_package "$apt_command"
+	pkg=$(grep "Remove" "$TEMP1")
+	pkg=${pkg#Remove: }
+	count_packages
+	((++count))
 	apt_command="Autoremove"
-	count=$((count += 1))
 	;;
-".*purge")
+" autopurge")
 	apt_command="Purge"
-	find_package "$apt_command"
+	pkg=$(grep "Purge" "$TEMP1")
+	pkg=${pkg#Purgee: }
+	count_packages
+	((++count))
+	apt_command="Autopurge"
 	;;
-".*remove")
+" purge")
+	apt_command="Purge"
+	find_package "$apt_command" "$apt_full_command" "$value"
+	;;
+" remove")
 	apt_command="Remove"
-	find_package "$apt_command"
+	find_package "$apt_command" "$apt_full_command" "$value"
 	;;
-".*reinstall")
+" reinstall")
 	apt_command="Reinstall"
-	find_package "$apt_command"
+	find_package "$apt_command" "$apt_full_command" "$value"
 	;;
-".*install")
+" install")
 	apt_command="Install"
-	find_package "$apt_command"
+	find_package "$apt_command" "$apt_full_command" "$value"
 	;;
 ".*-upgrade")
 	apt_command="DU"
 	apt_package=""
 	;;
-".*upgrade")
+" upgrade")
 	apt_command="Upgrade"
-	find_package "$apt_command"
+	apt_package=""
 	;;
-".*synaptic")
+" synaptic")
 	apt_command="synaptic"
 	apt_package=""
 	;;
@@ -147,7 +186,7 @@ esac
 # The apt times must be within those of snapper.
 if [ $pre_date -le $apt_start ] && [ $post_date -ge $apt_end ]; then
 	if [ "$count" -gt 1 ]; then
-		count=$((count -= 1))
+		((--count))
 		if [ "x$apt_command" = "xAutoremove" ]; then
 		apt_package="$count pkg"
 		else
@@ -161,4 +200,3 @@ else
 fi
 rm "${TEMP1}"
 exit 0
-
